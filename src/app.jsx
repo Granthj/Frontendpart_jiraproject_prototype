ebc46914faf788049c9979b888cd8af5964cd681
root <dasgranth24@gmail.com>
2025-08-13 22:20:23 +0530

    in / page i added link

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth } from "../src/Authentication/AuthContext";
-import { useNavigate } from 'react-router';
+// import { useNavigate } from 'react-router';
+import { Link,useNavigate } from 'react-router-dom';
 const App = () => {
     const { user } = useAuth();
     const { logout } = useAuth();
@@ -20,11 +21,21 @@ const App = () => {
                 <h1 className="text-primary">Welcome to the Assign Task</h1>
                 <p>Technologies used node,express,mongoDb and reactjs with jwt token</p>
                 <p>All routes details</p>
-                <p>/ for home page</p>
-                <p>/admin-dashboard for admin dashboard</p>
-                <p>/user-dashboard/:email for user dashboard</p>
-                <p>/login for login</p>
-                <p>/signup for signup</p>
+                <p>
+                    <Link to="/"></Link> for home page
+                </p>
+                <p>
+                    <Link to="/admin-dashboard"></Link> for admin dashboard (Not any authentication and login option yet)
+                </p>
+                <p>
+                    <Link to="/user-dashboard/:email"></Link> for user dashboard (Protected Route please login before)
+                </p>
+                <p>
+                    <Link to="/login"></Link> for login
+                </p>
+                <p>
+                    <Link to="/signup">/signup</Link> for signup
+                </p>
                 {user.email?(<button className="btn btn-success" onClick={handleLogout}>
                     Logout
                 </button>):(<button className="btn btn-success" onClick={handleLogin}>
